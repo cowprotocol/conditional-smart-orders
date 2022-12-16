9ca7dab8c564a3ec2afaae03d4864f1019db45e1
Felix Leupold <felixleupold90@gmail.com>
2022-12-16 17:43:50 +0100

    Implement Conditional order interface and concrete TradeAboveThreshold implementation (#1)
    This PR implements a first conditional order type (inspired by https://twitter.com/nicksdjohnson/status/1595866753288265729) and introduces a framework for many more of these order types (more details in the README).
    
    The "meat" here is 80 lines of solidity + 120 lines of test (the rest is largely project setup).

---
+++ README.md
@@ -0,0 +1,42 @@
+# Conditional Smart Orders
+
+Repository to showcase how to implement autonomous trading agents in the form of smart contracts that would like to place orders based on some observable on chain condition (e.g. token balance, oracle price, etc).
+
+This repository defined a common interface (`ConditionalOrder`) that all smart order contracts under this framework should follow. There are three main expectations
+
+1. Smart Order contracts emit an `ConditionalOrderCreated` event upon creation allowing for some backend infrastructure to index and monitor them.
+2. Whenever the contract would like to make a trade, `getTradeableOrder()` should return the CoW Protocol order data so that the monitoring tool can place an order in the backend accordingly.
+3. Smart orders implement ERC1271 to "validate" the placed CoW Protocol orders (`isValidSignature(digest, signature)`). Digest will be the typed EIP712 hash of the order data, signature will be the abi-encoded pre-image of the digest, in other words the data that `getTradeableOrder()` returned in the first place.
+
+***Note**: The contracts in this repository may not have been properly audited and could contain logic errors. Use at your own risk.*
+
+## Requirements
+
+- [Foundry](https://github.com/foundry-rs/foundry)
+- node (v16.18.0)
+- yarn
+
+## How to build/deploy an existing order type
+
+The repository comes with one example contracts:
+
+- **TradeAboveThreshold:** Checks the contract's balance of a specific token and - if larger than a threshold - wants to trade all of it into a specified buy token.
+
+Check `src/` for a full list and implementation details. In order to deploy one of the contracts for your own use case, execute the following steps:
+
+1. `forge build`
+2. `export PRIVATE_KEY=<...>`
+3. `export ETH_RPC_URL=<...>` (this will decide what network you deploy on)
+4. `forge create <ContractName> --private-key $PRIVATE_KEY --verify --constructor-args <constructor args>` (please refer to implementation for constructor arguments)
+
+The deployed contract should automatically be picked up by a watchdog and - once it returns a tradable order - find itself automatically placing this order. The status of placed orders by this contract can be observed on https://explorer.cow.fi/.
+
+## Writing your own Smart Order
+
+You are more than welcome to create new order types. For this simply create a new smart contract implementation in the `src/` folder (with an accompanying test contract in `/test`) and start hacking. 
+
+Your contract should inherit from `ConditionalOrder` and `EIP1271Verifier`. This ensures you are implementing the correct methods. If you want your contract to be picked up by a watchdog, don't forget to emit the `ConditionalOrderCreated` even in the constructor.
+
+Look at other example contracts for help/inspiration. To test your contract and make sure they comply with our style-guides please run:
+1. `forge test`
+2. `yarn fmt`
---
+++ test/TradeAboveThreshold.t.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma abicoder v2;
+pragma solidity ^0.7.6;
+
+import "forge-std/Test.sol";
+import "../src/TradeAboveThreshold.sol";
+
+contract TradeAboveThresholdTest is Test {
+    using GPv2Order for GPv2Order.Data;
+
+    TradeAboveThreshold public instance;
+    GPv2Settlement settlement;
+    IERC20 public sellToken;
+    IERC20 public buyToken;
+    address public receiver;
+
+    function setUp() public {
+        sellToken = IERC20(0x1);
+        buyToken = IERC20(0x2);
+        receiver = address(0x3);
+        settlement = new GPv2Settlement(GPv2Authentication(0), IVault(0));
+
+        //mock approval
+        vm.mockCall(
+            address(sellToken),
+            abi.encodeWithSelector(
+                sellToken.approve.selector,
+                settlement.vaultRelayer(),
+                uint(-1)
+            ),
+            abi.encode(true)
+        );
+
+        instance = new TradeAboveThreshold(
+            IERC20(sellToken),
+            IERC20(buyToken),
+            receiver,
+            100,
+            settlement
+        );
+    }
+
+    function setBalance(uint256 balance) private {
+        vm.mockCall(
+            address(sellToken),
+            abi.encodeWithSelector(sellToken.balanceOf.selector, instance),
+            abi.encode(balance)
+        );
+    }
+
+    function testRevertsWhenNotEnoughBalance() public {
+        setBalance(10);
+        vm.expectRevert(bytes("Not enough balance"));
+        instance.getTradeableOrder();
+    }
+
+    function testReturnsOrderIfEnoughBalance() public {
+        setBalance(100);
+        GPv2Order.Data memory order = instance.getTradeableOrder();
+        require(order.sellToken == sellToken);
+        require(order.buyToken == buyToken);
+        require(order.sellAmount == 100);
+        require(order.buyAmount == 1);
+    }
+
+    function testReturnsSameValidToAsLongAsInTheSameQuarterHour() public {
+        setBalance(100);
+
+        vm.warp(1670625300); // 22:35
+        GPv2Order.Data memory order = instance.getTradeableOrder();
+        require(order.validTo == 1670626800); //23:00
+
+        vm.warp(1670625600); // 22:40
+        order = instance.getTradeableOrder();
+        require(order.validTo == 1670626800); //23:00
+
+        vm.warp(1670626200); // 22:50
+        order = instance.getTradeableOrder();
+        require(order.validTo == 1670627700); //23:15
+    }
+
+    function testIsValidOrderWorksWithTradableOrder() public {
+        setBalance(200);
+        GPv2Order.Data memory order = instance.getTradeableOrder();
+        require(
+            instance.isValidSignature(
+                order.hash(settlement.domainSeparator()),
+                abi.encode(order)
+            ) == 0x1626ba7e
+        );
+    }
+
+    function testIsValidOrderRevertsWithMutatedOrder() public {
+        setBalance(200);
+        GPv2Order.Data memory order = instance.getTradeableOrder();
+        order.sellAmount += 1;
+
+        bytes32 hash = order.hash(settlement.domainSeparator());
+        bytes memory encoded = abi.encode(order);
+
+        vm.expectRevert("encoded order is not equal to tradable order");
+        instance.isValidSignature(hash, encoded);
+    }
+
+    function testIsValidOrderRevertsWithWrongPreimage() public {
+        setBalance(200);
+        GPv2Order.Data memory order = instance.getTradeableOrder();
+        bytes32 hash = order.hash(settlement.domainSeparator());
+
+        order.sellAmount += 1;
+        bytes memory encoded = abi.encode(order);
+
+        vm.expectRevert("encoded order doesn't match digest");
+        instance.isValidSignature(hash, encoded);
+    }
+}
